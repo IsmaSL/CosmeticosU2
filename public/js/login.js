3eca2d7c4cde8eccf0d593dfdce41ade06c14e6d
Miguelsv21 <mangelenerk@gmail.com>
2019-10-25 21:37:37 -0500

    Todas las funciones fueron modificas de js a jquery, excepto llamaLogin

--- public/js/login.js
+++ public/js/login.js
@@ -1,158 +1,162 @@
-function muestraLogin() {
-    var divTexto = document.getElementById("divTexto");
-    var divDesliza = document.getElementById("divDesliza");
-    var sectionLogin = document.getElementById("sectionLogin");
-    var sectionRegistrarse = document.getElementById("sectionRegistrarse");
-    var regProducto = document.getElementById("regProducto");
-    var divParentAbsoluto = document.getElementById('parentAbsoluto');
-    var welcome = document.getElementById('welcome');
-    //aquí debe poner el login en display:none y el welcome en block
-    //ya está el json cargado con la información, por si necesitas sacar algo
-    if (divTexto.style.display === "flex" || divParentAbsoluto.style.display === "flex" ||
-        regProducto.style.display === "flex" || sectionRegistrarse.style.display === "flex" ||
-        welcome.style.display === "flex") {
-        welcome.style.display = "none";
-        divTexto.style.display = "none";
-        sectionLogin.style.display = "flex";
-        divParentAbsoluto.style.display = "none";
-        sectionRegistrarse.style.display = "none";
-        regProducto.style.display = "none";
-        divDesliza.style.display = "none";
+
+    function muestraLogin() {
+        var divTexto = $('#divTexto');
+        var divDesliza = $("#divDesliza");
+        var sectionLogin = $("#sectionLogin");
+        var sectionRegistrarse = $("#sectionRegistrarse");
+        var regProducto = $("#regProducto");
+        var divParentAbsoluto = $('#parentAbsoluto');
+        var welcome = $('#welcome');
+        //aquí debe poner el login en display:none y el welcome en block
+        //ya está el json cargado con la información, por si necesitas sacar algo
+        if (divTexto.css("display") === "flex" || divParentAbsoluto.css("display") === "flex" ||
+            regProducto.css("display") === "flex" || sectionRegistrarse.css("display") === "flex" ||
+            welcome.css("display") === "flex") {
+            welcome.hide();
+            divTexto.hide();
+            sectionLogin.css("display","flex");
+            divParentAbsoluto.hide();
+            sectionRegistrarse.hide();
+            regProducto.hide();
+            divDesliza.hide();
+        }
     }
-}
 
-function cambiaForms(){
-    var sectionLogin=document.getElementById("sectionLogin");
-    var sectionRegistrarse=document.getElementById("sectionRegistrarse");
-    var divDesliza = document.getElementById("divDesliza");
-    if(sectionLogin.style.display==="flex"){
-        sectionLogin.style.display="none";
-        divDesliza.style.display = "none";
-        sectionRegistrarse.style.display="flex";
-        document.getElementById("formLogin").reset();
-    }else{
-        sectionLogin.style.display="flex";
-        sectionRegistrarse.style.display="none";
-        divDesliza.style.display = "none";
+    function cambiaForms(){
+        var sectionLogin=$("#sectionLogin");
+        var sectionRegistrarse=$("#sectionRegistrarse");
+        var divDesliza = $("#divDesliza");
+        if(sectionLogin.css("display")==="flex"){
+            sectionLogin.hide();
+            divDesliza.hide();
+            sectionRegistrarse.css("display","flex");
+            $("#formLogin").trigger("reset");
+        }else{
+            sectionLogin.css("display","flex");
+            sectionRegistrarse.hide();
+            divDesliza.hide();
+        }
     }
-}
 
-function llamaLogin(cuentaUsuario, contrasenia){
-	var request = new XMLHttpRequest();
-	var url = "https://cosmeticosu2.dev/api/login";
-	var sQueryString="";
+    function llamaLogin(cuentaUsuario, contrasenia){
+        var request = new XMLHttpRequest();
+        var url = "/api/login";
+        var sQueryString="";
 
-	if (cuentaUsuario=="" || contrasenia==""){
-		alert("Faltan datos para el ingreso");
-	}else{
-		sQueryString="cuentaUsuario="+cuentaUsuario+"&contrasenia="+contrasenia;
-		request.onreadystatechange=function() {
-			if (request.readyState === 4 && request.status === 200) {
-			    loaderLogin.style.display = "none";
-				procesaLogin(request.responseText);
-			}else{
-				if (request.status != 200 &&request.status != 0){
-					var msj = JSON.parse(request.responseText);
-					alert(msj.mensaje);
+        if (cuentaUsuario=="" || contrasenia==""){
+            alert("Faltan datos para el ingreso");
+        }else{
+            sQueryString="cuentaUsuario="+cuentaUsuario+"&contrasenia="+contrasenia;
+            request.onreadystatechange=function() {
+                if (request.readyState === 4 && request.status === 200) {
                     loaderLogin.style.display = "none";
-				}
-			}
-		};
-		request.open("POST", url, true);
-		request.setRequestHeader("Content-type",
-		"application/x-www-form-urlencoded");
-		request.send(sQueryString);
+                    procesaLogin(request.responseText);
+                }else{
+                    if (request.status != 200 &&request.status != 0){
+                        var msj = JSON.parse(request.responseText);
+                        alert(msj.mensaje);
+                        loaderLogin.style.display = "none";
+                    }
+                }
+            };
+            request.open("POST", url, true);
+            request.setRequestHeader("Content-type",
+            "application/x-www-form-urlencoded");
+            request.send(sQueryString);
 
-		loaderLogin.style.display = "flex";
-	}
-}
-
-function verificaStorage(nombre, tipoUsuario) {
-    var divTexto = document.getElementById("divTexto");
-    var divWelcome1=document.getElementById("welcome");
-    var navmen = document.getElementById("sp");
-    var logout = document.getElementById("logout");
-    var sectionLogin = document.getElementById("sectionLogin");
-    var sectionRegistrarse = document.getElementById("sectionRegistrarse");
-    var ml = document.getElementById("muestraLogin");
-
-    logout.style.display="flex";
-    navmen.innerHTML=nombre;
-    if(tipoUsuario=='A'){
-    	var regProd = document.getElementById('regProd');
-    	regProd.style.display = "flex";
+            loaderLogin.style.display = "flex";
+        }
     }
-    ml.href="#";
-    divTexto.style.display="none";
-    sectionLogin.style.display="none";
-    sectionRegistrarse.style.display="none";
-    divWelcome1.style.display="flex";
-    sectionLogin.style.display="none";
-    sectionRegistrarse.style.display="none"
-}
 
-function procesaLogin(textoRespuesta){
-    var datos = JSON.parse(textoRespuesta);
-	var divWelcome1=document.getElementById("welcome");
-	var navmen = document.getElementById("sp");
-	var logout = document.getElementById("logout");
-    var sectionLogin = document.getElementById("sectionLogin");
-	var sectionRegistrarse = document.getElementById("sectionRegistrarse");
-	var ml = document.getElementById("muestraLogin");
-	if(datos.tipoUsuario=='A'){
-    	var regProd = document.getElementById('regProd');
-    	regProd.style.display = "flex";
+    function verificaStorage(nombre, tipoUsuario) {
+        var divTexto = $("#divTexto");
+        var divWelcome1=$("#welcome");
+        var navmen = $("#sp");
+        var logout = $("#logout");
+        var sectionLogin = $("#sectionLogin");
+        var sectionRegistrarse = $("#sectionRegistrarse");
+        var ml = $("#muestraLogin");
+
+        logout.css("display","flex");
+        navmen.html(nombre);
+        if(tipoUsuario=='A'){
+            var regProd = $('#regProd');
+            regProd.css("display","flex");
+        }
+        ml.attr("href","#");
+        divTexto.hide();
+        sectionLogin.hide();
+        sectionRegistrarse.hide()
+        divWelcome1.css("display","flex");
+        sectionLogin.hide();
+        sectionRegistrarse.hide();
     }
-	//Guarda en la sessionStorage
-	sessionStorage.setItem("nombre", datos.nombre);
-    sessionStorage.setItem("primApellido", datos.primApellido);
-    sessionStorage.setItem("segApellido", datos.segApellido);
-    sessionStorage.setItem("tipoUsuario", datos.tipoUsuario);
 
-	logout.style.display="flex";
-	navmen.innerHTML=datos.nombre;
-	ml.href="#";
-	//aquí debe poner el login en display:none y el welcome en block
-	//ya está el json cargado con la información, por si necesitas sacar algo
-	if(sectionLogin.style.display==="flex"){
-        sectionLogin.style.display="none";
-        sectionRegistrarse.style.display="none";
-        divWelcome1.style.display="flex";
-    }else{
-        sectionLogin.style.display="flex";
-        sectionRegistrarse.style.display="none"
+    function procesaLogin(textoRespuesta){
+        var datos = JSON.parse(textoRespuesta);
+        var divWelcome1=$("#welcome");
+        var navmen = $("#sp");
+        var logout = $("#logout");
+        var sectionLogin = $("#sectionLogin");
+        var sectionRegistrarse = $("#sectionRegistrarse");
+        var ml = $("#muestraLogin");
+        if(datos.tipoUsuario=='A'){
+            var regProd = $('#regProd');
+            regProd.css("display","flex");
+        }
+        //Guarda en la sessionStorage
+        sessionStorage.setItem("nombre", datos.nombre);
+        sessionStorage.setItem("primApellido", datos.primApellido);
+        sessionStorage.setItem("segApellido", datos.segApellido);
+        sessionStorage.setItem("tipoUsuario", datos.tipoUsuario);
+
+        logout.css("display","flex");
+        navmen.html(datos.nombre);
+        ml.attr("href","#");
+        //aquí debe poner el login en display:none y el welcome en block
+        //ya está el json cargado con la información, por si necesitas sacar algo
+        if(sectionLogin.css("display")==="flex"){
+            sectionLogin.hide();
+            sectionRegistrarse.hide();
+            divWelcome1.css("display","flex");
+        }else{
+            sectionLogin.css("display","flex");
+            sectionRegistrarse.hide();
+        }
     }
-}
 
-function cerrarSesion(){
-	sessionStorage.clear();
-	var logout = document.getElementById("logout");
-	var navmen = document.getElementById("sp");
-	var divTexto = document.getElementById("divTexto");
-	var divDesliza = document.getElementById("divDesliza");
-    var divParentAbsoluto = document.getElementById('parentAbsoluto');
-	var login = document.getElementById("sectionLogin");
-	var ml = document.getElementById("muestraLogin");
-	var divWelcome1=document.getElementById("welcome");
-	var regProd = document.getElementById('regProd');
-    var regProducto = document.getElementById("regProducto");
-	if(regProd.style.display = "flex"){
-    	regProd.style.display = "none";
-        regProducto.style.display = "none";
-        divDesliza.style.display = "none";
+    function cerrarSesion(){
+        sessionStorage.clear();
+        var logout = $("#logout");
+        var navmen = $("#sp");
+        var divTexto = $("#divTexto");
+        var divDesliza = $("#divDesliza");
+        var divParentAbsoluto = $('#parentAbsoluto');
+        var login = $("#sectionLogin");
+        var ml = $("#muestraLogin");
+        var divWelcome1=$("#welcome");
+        var regProd = $('#regProd');
+        var regProducto = $("#regProducto");
+        if(regProd.css("display") === "flex"){
+            regProd.hide();
+            regProducto.hide();
+            divDesliza.hide();
+        }
+        divWelcome1.hide();
+        ml.attr("href","javascript:muestraLogin();");
+        logout.hide();
+        navmen.html("Iniciar sesi&oacute;n");
+        divParentAbsoluto.hide();
+        login.hide();
+
+        divTexto.css("display","flex");
+        $("#formLogin").trigger("reset");
     }
-	divWelcome1.style.display="none";
-	ml.href="javascript:muestraLogin();";
-	logout.style.display="none";
-    navmen.innerHTML="Iniciar sesi&oacute;n";
-    divParentAbsoluto.style.display="none";
-	login.style.display="none";
 
-	divTexto.style.display="flex";
-	document.getElementById("formLogin").reset();
-}
+    function registraUsuario(){
+        var mensajeRegisttro = $("#mensajeRegistro");
+        mensajeRegisttro.css("display","flex");
+    }
 
-function registraUsuario(){
-    var mensajeRegisttro = document.getElementById("mensajeRegistro");
-    mensajeRegisttro.style.display="flex";
-}
+
+
